9939690aa49f3b3129f9b9fe0563f0cf3202829e
YoungKong <84266008+YoungKong@users.noreply.github.com>
2021-06-02 18:47:27 +0900

    6:47

---
+++ SecondPageMain.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class SecondPageMain extends JFrame{
+	Container content;
+	SecondPage imgP;
+	
+	public SecondPageMain(String title){
+		//super(title);
+		imgP = new SecondPage();
+		content = getContentPane();
+		content.add(imgP, BorderLayout.CENTER);
+		setSize(1100,800);
+		setVisible(true);
+	}
+    public static void main(String[] args) {
+        SecondPageMain sp = new SecondPageMain("청파버거");
+    }
+}
