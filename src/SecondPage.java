097d429593ffe03539deb123340f8686cf126161
seyoungkim1715231 <85218841+seyoungkim1715231@users.noreply.github.com>
2021-06-04 09:02:41 +0900

    접시에 담고 맞는지 비교까지 0 틀리면 주문서 새로 들어옴

--- src/SecondPage.java
+++ src/SecondPage.java
@@ -14,6 +14,10 @@ public class SecondPage extends JPanel implements ActionListener {
     TotalTimer tt = new TotalTimer();
     MenuTimer mt = new MenuTimer();
     Random_ordersheet rp=new Random_ordersheet();
+	
+	int i=0;
+
+	
     final Image topbun = new ImageIcon("topbun.png").getImage();
 	final Image bottombun = new ImageIcon("bottombun.png").getImage();
 	final Image cheese = new ImageIcon("cheese.png").getImage();
@@ -25,13 +29,15 @@ public class SecondPage extends JPanel implements ActionListener {
 	int r1,r2,r3,r4,r5;
 	int level=1;
 	Image [] recipe= new Image[7];
+	ImageIcon [] making=new ImageIcon[7];
+	JLabel one,two,three,four,five,six,seven;
    
     public SecondPage(int level){    
   		
     	back = new ImageIcon("back.png").getImage();
     	setLayout(null);
 
-        //ìƒëª… 1~5ê°œ
+        //»ý¸í 1~5°³
         JLabel life1  = new JLabel();
         life1.setIcon(new ImageIcon("life.png"));
         JLabel life2  = new JLabel();
@@ -43,33 +49,54 @@ public class SecondPage extends JPanel implements ActionListener {
         JLabel life5  = new JLabel();
         life5.setIcon(new ImageIcon("life.png"));
 
-        //ì ‘ì‹œ ì´ë¯¸ì§€
+        //Á¢½Ã ÀÌ¹ÌÁö
         JLabel dish = new JLabel();
         dish.setIcon(new ImageIcon("dish.png"));
         JLabel ordersheet = new JLabel();
         ordersheet.setIcon(new ImageIcon("order.png"));
+		one  = new JLabel();//Á¢½Ã¿¡ ¿Ã¶ó°¥ ¶óº§
+		two  = new JLabel();
+		three  = new JLabel();
+		four  = new JLabel();
+		five  = new JLabel();
+		six  = new JLabel();	
+		seven  = new JLabel();
         
-        
-        
-        //í–„ë²„ê±° ìž¬ë£Œ ë²„íŠ¼ //ì•ˆì— ì´ë¯¸ì§€ íŒŒì¼ ì¶”ê°€í•˜ê¸°.
+        //ÇÜ¹ö°Å Àç·á ¹öÆ° //¾È¿¡ ÀÌ¹ÌÁö ÆÄÀÏ Ãß°¡ÇÏ±â.
         b_topbun = new JButton(new ImageIcon("topbun.png")); 
         b_bottombun = new JButton(new ImageIcon("bottombun.png"));
         b_lettuce = new JButton(new ImageIcon("lettuce.png"));
         b_tomato = new JButton(new ImageIcon("tomato.png"));
         b_cheese = new JButton(new ImageIcon("cheese.png"));
         b_patty = new JButton(new ImageIcon("patty1.png"));
-        b_patty.addActionListener((ActionListener) this);
+        
         b_onion = new JButton(new ImageIcon("onion.png"));
         b_coke = new JButton(new ImageIcon("coke.png"));
         b_sprite = new JButton(new ImageIcon("sprite.png"));
         pan = new JLabel(new ImageIcon("pan.png"));
+		
+		b_patty.addActionListener((ActionListener) this);
+
+		b_topbun.addActionListener((ActionListener) this);
+		b_bottombun.addActionListener((ActionListener) this);
+		b_lettuce.addActionListener((ActionListener) this);
+		b_tomato.addActionListener((ActionListener) this);
+		b_cheese.addActionListener((ActionListener) this);
+		b_onion.addActionListener((ActionListener) this);
+		b_coke.addActionListener((ActionListener) this);
+		b_sprite.addActionListener((ActionListener) this);
+
      	
         patty_pan = new JLabel();
     	patty_pan.setBounds(140,570,200,100);
  
         add(tt);
         add(mt);
-    	add(rp);
+    	//if(mt.menu==1){
+		//	add(rp);
+		//}
+		add(rp);
+		
     	add(ordersheet);
     	add(patty_pan);
         add(b_topbun);
@@ -88,13 +115,22 @@ public class SecondPage extends JPanel implements ActionListener {
         add(life3);
         add(life4);
         add(life5);
-        add(dish);
+		
+		add(seven);
+		add(six);
+		add(five); 
+		add(four);
+		add(three);
+		add(two);
+		add(one);
+		add(dish);
+
         
         rp.setBounds(0, -10, 300, 300);
         mt.setBounds(700, 450, 300, 135);
         tt.setBounds(700, 615, 300, 135);
         
-        //(x, y, ê°€ë¡œ, ì„¸ë¡œ)
+        //(x, y, °¡·Î, ¼¼·Î)
 		b_topbun.setBounds(370, 30, 135, 65);
 		b_topbun.setBorderPainted(false);
 		b_topbun.setBackground(Color.white);
@@ -139,9 +175,21 @@ public class SecondPage extends JPanel implements ActionListener {
         life3.setBounds(828, 370, 60, 50);
         life4.setBounds(894, 370, 60, 50);
         life5.setBounds(958, 370, 60, 50);
-        dish.setBounds(150,400, 600, 500);
+        
         ordersheet.setBounds(-275, 20, 700, 300);
         patty_img = new ImageIcon("patty1.png");
+		
+		one.setBounds(410,570, 170, 135);//Å¬¸¯ÇÑ Àç·á°¡ Á¢½ÃÀ§¿¡ ½×ÀÓ
+		two.setBounds(410,550, 170, 135);
+		three.setBounds(410,530, 170, 135);
+		four.setBounds(410,510, 170, 135);
+		five.setBounds(410,490, 170, 135);
+		six.setBounds(410,470, 170, 135);
+		seven.setBounds(410,450, 170, 135);
+		dish.setBounds(150,410, 600, 500);
+		
+		
+
     }
     
     public void paint(Graphics g) {
@@ -151,6 +199,224 @@ public class SecondPage extends JPanel implements ActionListener {
     }
     
     public void actionPerformed(ActionEvent e) {
-    	patty_pan.setIcon(patty_img);
+    	
+		if(e.getSource()==b_patty) { //ÆÐÆ¼°¡ ÈÄ¶óÀÌÆÒ¿¡ µé¾î°¡¸é ÆÐÆ¼ Å¸ÀÌ¸Ó °¡µ¿ÇÏ±â(¹Ì¿Ï), ÆÒ¿¡ ÀÖ´Â ÆÐÆ¼¸¦ ´­·¯¾ß Á¢½Ã¿¡ ½×ÀÓ(¹Ì¿Ï)
+			patty_pan.setIcon(patty_img);
+		}
+		if(e.getSource()==b_topbun) {
+			making[i]=new ImageIcon("topbun.png");
+			makelabel(i);
+			if(rp.recipe[i]==making[i].getImage()){//¿©±â if¹®À» ÇÔ¼ö·Î ¸¸µå´Ï±î i++¶§¹®¿¡ ÀÎµ¦½º1¹øºÎÅÍ Å¬¸¯¹Ý¿µÀÌ ¾ÈµÊ.
+				System.out.println("¸Â´Â Àç·áÀÔ´Ï´Ù.");
+				i++;	
+			}
+		
+			else if (rp.recipe[i]!=making[i].getImage())
+			{
+				System.out.println("Æ²·È½À´Ï´Ù.");
+				//clear(); Æ²¸±°æ¿ì ÁÖ¹®¼­´Â ´Ù½Ã ±×·ÁÁöÁö¸¸ Á¢½ÃÀÇ ¶óº§Àº Áö¿öÁöÁö¾ÊÀ½. clear()·Î Áö¿öÁ®µµ ´Ù½Ã ±×·ÁÁöÁö¾ÊÀ½
+				i=0;
+				rp.repaint();//Æ²¸±°æ¿ì ÁÖ¹®¼­ ´Ù½Ã±×¸²
+
+			}
+			//rule(i);
+			
+		}
+		if(e.getSource()==b_bottombun) {
+			making[i]=new ImageIcon("bottombun.png");
+			makelabel(i);
+			if(rp.recipe[i]==making[i].getImage()){
+				System.out.println("¸Â´Â Àç·áÀÔ´Ï´Ù.");
+				i++;	
+			}
+		
+			else if (rp.recipe[i]!=making[i].getImage())
+			{
+				System.out.println("Æ²·È½À´Ï´Ù.");
+				//clear();
+				i=0;
+				rp.repaint();
+
+			}
+			//rule(i);
+		}
+		if(e.getSource()==patty_pan) {
+			making[i]=new ImageIcon("patty2.png");
+			makelabel(i);
+			if(rp.recipe[i]==making[i].getImage()){//¿©±â if¹®À» ÇÔ¼ö·Î ¸¸µå´Ï±î i++¶§¹®¿¡ ÀÎµ¦½º1¹øºÎÅÍ Å¬¸¯¹Ý¿µÀÌ ¾ÈµÊ.
+				System.out.println("¸Â´Â Àç·áÀÔ´Ï´Ù.");
+				i++;	
+			}
+		
+			else if (rp.recipe[i]!=making[i].getImage())
+			{
+				System.out.println("Æ²·È½À´Ï´Ù.");
+				//clear();
+				i=0;
+				rp.repaint();
+
+			}
+			//rule(i);
+			
+		}
+		if(e.getSource()==b_lettuce) {
+			making[i]=new ImageIcon("lettuce.png");
+			makelabel(i);
+			if(rp.recipe[i]==making[i].getImage()){
+				System.out.println("¸Â´Â Àç·áÀÔ´Ï´Ù.");
+				i++;	
+			}
+		
+			else if (rp.recipe[i]!=making[i].getImage())
+			{
+				System.out.println("Æ²·È½À´Ï´Ù.");
+				//clear();
+				i=0;
+				rp.repaint();
+
+			}
+			//rule(i);
+		
+		}
+		if(e.getSource()==b_cheese) {
+			making[i]=new ImageIcon("cheese.png");
+			makelabel(i);
+			if(rp.recipe[i]==making[i].getImage()){
+				System.out.println("¸Â´Â Àç·áÀÔ´Ï´Ù.");
+				i++;	
+			}
+		
+			else if (rp.recipe[i]!=making[i].getImage())
+			{
+				System.out.println("Æ²·È½À´Ï´Ù.");
+				//clear();
+				i=0;
+				rp.repaint(); 
+
+			}
+			//rule(i);
+		
+		}
+		if(e.getSource()==b_onion) {
+			making[i]=new ImageIcon("onion.png");
+			makelabel(i);
+			if(rp.recipe[i]==making[i].getImage()){
+				System.out.println("¸Â´Â Àç·áÀÔ´Ï´Ù.");
+				i++;	
+			}
+		
+			else if (rp.recipe[i]!=making[i].getImage())
+			{
+				System.out.println("Æ²·È½À´Ï´Ù.");
+				//clear();
+				i=0;
+				rp.repaint();
+
+			}
+			//rule(i);
+		}
+		if(e.getSource()==b_tomato) {
+			making[i]=new ImageIcon("tomato.png");
+			makelabel(i);
+			if(rp.recipe[i]==making[i].getImage()){
+				System.out.println("¸Â´Â Àç·áÀÔ´Ï´Ù.");
+				i++;	
+			}
+		
+			else if (rp.recipe[i]!=making[i].getImage())
+			{
+				System.out.println("Æ²·È½À´Ï´Ù.");
+				i=0;
+				//clear();
+				rp.repaint();
+
+			}
+			//rule(i);
+		
+		}
+		if(e.getSource()==b_coke) {
+			
+		}
+		if(e.getSource()==b_sprite) {
+			
+		}
     }
+	
+	
+
+	public void makelabel(int i){
+			if (i==0){
+				one.setIcon(making[i]);
+			}
+			if (i==1){
+				two.setIcon(making[i]);
+			}
+			if (i==2){
+				three.setIcon(making[i]);
+			}
+			if (i==3){
+				four.setIcon(making[i]);
+			}
+			if (i==4){
+				five.setIcon(making[i]);
+			}
+			if (i==5){
+				six.setIcon(making[i]);
+			}
+			if (i==6){
+				seven.setIcon(making[i]);
+			}
+
+	}
+
+	public int rule(int i){
+		if(rp.recipe[i]==making[i].getImage()){
+				System.out.println("¸Â´Â Àç·áÀÔ´Ï´Ù.");
+				i++;	
+				if(level==1){//level1ÀÇ °æ¿ì ÀÎµ¦½º°¡ 4ÀÌ»ó±îÁö ¸Â°Ô Çß´Ù¸é ¹è¿­À» ÃÊ±âÈ­ÇÏ°í ´Ù½Ã ÁÖ¹®¼­·ê ºÒ·¯¿È
+					if(i>4){
+						i=0;
+						clear();
+						add(rp);
+						
+					}
+				}
+				if(level==2){
+					if(i>5){
+						i=0;
+						clear();
+						add(rp);
+						
+					}
+				}
+				if(level==3){
+					if(i>6){
+						i=0;
+						clear();
+						add(rp);
+						
+					}
+				}
+		}
+		else if (rp.recipe[i]!=making[i].getImage())
+		{
+				System.out.println("Æ²·È½À´Ï´Ù.");
+				i=0;
+				clear();
+				add(rp);
+		}
+
+		return i;
+	}
+
+	public void clear(){
+		remove(one);
+		remove(two);
+		remove(three);
+		remove(four);
+		remove(five);
+		remove(six);
+		remove(seven);
+	}
+
 }
