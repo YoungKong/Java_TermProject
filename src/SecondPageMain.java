3a1aba1f416cc036f35b00c1c1fca2ec84cbd4e6
mmnmm8 <85218080+mmnmm8@users.noreply.github.com>
2021-06-05 01:27:20 +0900

    int level 수정

--- src/SecondPageMain.java
+++ src/SecondPageMain.java
@@ -4,17 +4,22 @@ import javax.swing.*;
 public class SecondPageMain extends JFrame{
 	Container content;
 	SecondPage imgP;
+//	public int restart=0;
 	
-	public SecondPageMain(String title){
-		super(title);
-		imgP = new SecondPage(1);
+	public SecondPageMain(int level){
+		
+		super("청파버거");
+		imgP = new SecondPage(level);
 		content = getContentPane();
 		content.add(imgP, BorderLayout.CENTER);
 		setSize(1100,800);
+		setLocation(200,30);
 		setVisible(true);
+		//end();
+		//System.out.println(restart);
+
 	}
+
+//	reset++;
 	
-    public static void main(String[] args) {
-        SecondPageMain sp = new SecondPageMain("Burger");
-    }
 }
