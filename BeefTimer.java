d96fe2b435d93c618ab1d71ca3af55215834bf9c
knyoon <80137728+knyoon@users.noreply.github.com>
2021-06-10 17:13:41 +0900

    Update BeefTimer.java
    beeftimer 수정 완료

--- BeefTimer.java
+++ BeefTimer.java
@@ -1,48 +1,37 @@
-package src; 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
-
-import java.awt.Color;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class BeefTimer extends JPanel{ 
-   
-    public static int count;
-    JLabel beef;
+public class BeefTimer extends Thread { 
+    public int count;
     
-    public BeefTimer(){
+    public BeefTimer(){       
         count = 0;
-        beef=new JLabel();
-        this.setBounds(140,570,200,100);
-        this.setBackground(new Color(0, 0, 0, 0));
-        this.add(beef);
-        this.setVisible(true);
-        
         Timer cook_timer = new Timer();
         TimerTask cook_task = new TimerTask(){
 
             @Override
             public void run(){
-                    if(count < 2){  //0~2초: 패티 안익음
-                        beef.setIcon(new ImageIcon("patty1.png"));
-                        count++;
+                if (count ==0 || count ==1 ){
+                   // System.out.println(count);
+                    //la.setIcon(new ImageIcon("patty1.png"));
+                    count++;
                     }
-                    else if (count < 4){ //2~4초: 패티 익음
-                        beef.setIcon(new ImageIcon("patty2.png"));
-                        count++;
+                else if(count == 2 || count ==3){
+                    //System.out.println(count);
+                    //la.setIcon(new ImageIcon("patty1.png"));
+                    count++;
                     }
-                    else{ 
-                        beef.setIcon(new ImageIcon("patty3.png"));
-                        cook_timer.cancel();
-                    }
+                else if(count==4){
+                    //System.out.println(count);
+                    //la.setIcon(new ImageIcon("patty3.png"));
+                    cook_timer.cancel();
+                }
                 }
             };
-            cook_timer.schedule(cook_task, 6000, 1000); //카운터 5이상이면 타이머 종료, 반복주기 1초
+            cook_timer.schedule(cook_task, 1000, 1000); 
     }
-//    public static void main(String[] args) {        
-//        BeefTimer bf = new BeefTimer();
-//    }
+
 }
